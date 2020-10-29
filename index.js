0ed4d9dfd918d2134a8ab9dcd1ab37e4b22e96d9
railson150 <railsontutoriais@gmail.com>
2020-10-29 01:53:34 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+
+
+app.get("/", (req, res) => {
+    res.send("Bem vindo ao meu site");
+})
+
+app.listen(8080, () => {
+    console.log("Servidor rodando")
+})
