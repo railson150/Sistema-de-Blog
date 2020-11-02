30cedcd24a531c2c8f0f89760e5436cbbb2c566d
railson150 <railsontutoriais@gmail.com>
2020-11-02 16:08:48 -0300

    Adiciona Página de cadastro de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router = express.Router()
-
-router.get('/categories', (req, res) => {
-    res.send(("Rota de categorias"))
-})
+const Category = require('./Category')
+const slugify = require('slugify')
 
 router.get('/admin/categories/new', (req, res) => {
-    res.send(("Rota para criar uma nova categorias"))
+    res.render('admin/categories/new')
+})
+
+router.post('/categories/save', (req, res) => {
+    var title = req.body.title
+    if(title != undefined) {
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect('/')
+        })
+    } else {
+        res.redirect("/admin/categories/new")
+    }
 })
 
 module.exports = router
